40183c0c97372068b5b154ecf085b112cb66a2df
NUVOTON\WHTU <whtu@nuvoton.com>
2023-08-31 11:00:53 +0800

    [version] Update version code to V1.07.0002.
    1. match to exe version.

--- src/NUC_Clock_Configuration.js
+++ src/NUC_Clock_Configuration.js
@@ -4,7 +4,7 @@ var NUTOOL_CLOCK = {};
 
 (function () {
     // all the global variables within the immediately-invoked function scope
-    const VERSION_CODE = 'V2.00.0002';
+    const VERSION_CODE = 'V1.07.0002';
     var g_NUC_TreeView_Width = 250,
         g_NUC_TreeView_Height,
         g_Dialog_Width,
