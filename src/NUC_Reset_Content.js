9b1c02cb8eedf59e48a81e3054879a03b8e10430
MS60 IJTai <IJTAI@nuvoton.com>
2024-11-29 13:25:11 +0800

    Fix the loading error after changing the register value

--- src/NUC_Reset_Content.js
+++ src/NUC_Reset_Content.js
@@ -1,8 +1,10 @@
-NUTOOL_CLOCK.g_BusFromACLK = [];
-NUTOOL_CLOCK.g_BusFromHCLK0 = [];
-NUTOOL_CLOCK.g_BusFromHCLK1 = [];
-NUTOOL_CLOCK.g_BusFromHCLK2 = [];
-NUTOOL_CLOCK.g_BusFromPCLK1 = [];
-NUTOOL_CLOCK.g_BusFromPCLK2 = [];
-NUTOOL_CLOCK.g_BusFromPCLK3 = [];
-NUTOOL_CLOCK.g_BusFromPCLK4 = [];
+NUTOOL_CLOCK.clearVars = function() {
+    NUTOOL_CLOCK.g_BusFromACLK = [];
+    NUTOOL_CLOCK.g_BusFromHCLK0 = [];
+    NUTOOL_CLOCK.g_BusFromHCLK1 = [];
+    NUTOOL_CLOCK.g_BusFromHCLK2 = [];
+    NUTOOL_CLOCK.g_BusFromPCLK1 = [];
+    NUTOOL_CLOCK.g_BusFromPCLK2 = [];
+    NUTOOL_CLOCK.g_BusFromPCLK3 = [];
+    NUTOOL_CLOCK.g_BusFromPCLK4 = [];
+};
